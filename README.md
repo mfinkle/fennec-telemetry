c68ce8e7b8bd5cf444e3ed395fdd780bfae558e7
Josh Dover <gerfuls@gmail.com>
2014-03-25 16:31:41 -0700

    Add script options, update README

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 fennec-telemetry
 ================
 
-scripts to be run on mozilla/telemetry-server for grabbing fennec telemetry
+MapReduce jobs to be run on mozilla/telemetry-server for grabbing fennec telemetry.
+
+To run a MapReduce job:
+
+1. Visit http://telemetry-dash.mozilla.org/ and launch a worker with your public SSH key
+1. Login to machine with SSH command given (wait about 5 mins for keys to propogate)
+1. Clone this repo in the home directory and run the job:
+```shell
+$ git clone https://github.com/gerfuls/fennec-telemetry.git
+# with my_job.py and my_filter.json as files in jobs/ and filters/
+$ ./fennec-telemetry/run.sh -j my_job -f my_filter
+```
+1. Wait
+1. Get results!
+
+See all options: `./fennec-telemetry/run.sh -h`
+
+Make a pull request if you would like to add a new job or filter!
+
+## Useful Links
+- [AustralisTelemetry](https://github.com/bwinton/AustralisTelemetry)
+- [mreid's blog](http://mreid-moz.github.io/blog/2013/11/06/current-state-of-telemetry-analysis/)
+- [MapReduce Docs](https://github.com/mozilla/telemetry-server/blob/master/docs/MapReduce.md)
--- run.sh
+++ run.sh
@@ -10,39 +10,59 @@ function setup_directories {
 }
 
 function show_usage {
-  echo "Usage: ./run -j job -f filter [OPTIONS]"
+  echo "Usage: $0 -j job -f filter [OPTIONS]"
   printf "Options:\n"
-  printf "\t\t -m: number of mappers (defaults to $DEFAULT_NUM_MAPPERS)\n"
-  printf "\t\t -n: number of reducers (defaults to $DEFAULT_NUM_REDUCERS)\n"
-  printf "\t\t -l: use already downloaded data (for when running subsequent jobs)"
+  printf "\t -h,  show this dialog\n"
+  printf "\t -j,  job name\n"
+  printf "\t -f,  filter name\n"
+  printf "\t -m,  number of mappers (defaults to $DEFAULT_NUM_MAPPERS)\n"
+  printf "\t -r,  number of reducers (defaults to $DEFAULT_NUM_REDUCERS)\n"
+  printf "\t -p,  force to redownload data from S3 (takes longer)\n"
   exit 1
 }
 
-function set_parameters {
+function set_defaults {
   NUM_MAPPERS=$DEFAULT_NUM_MAPPERS
   NUM_REDUCERS=$DEFAULT_NUM_REDUCERS
-  LOCAL=false
 
-  TEMP=`getopt j:f:m::r::l $@`
+  if [ -d "/mnt/telemetry/cache/saved_session" ]; then
+    PULL="--local-only"
+  else
+    PULL=""
+  fi
+}
+
+function set_parameters {
+  set_defaults
+
+  TEMP=`getopt j:f:m::r::ph $@`
   eval set -- "$TEMP"
 
   while true ; do
     case "$1" in
-      -j)
-        JOB_NAME=$2 ;
-        shift 2;;
-      -f)
-        FILTER_NAME=$2 ;
-        shift 2;;
-      -m)
-        NUM_MAPPERS=$2 ;
-        shift 2;;
-      -r)
-        NUM_REDUCERS=$2 ;
-        shift 2;;
-      -l)
-        LOCAL=true ;
-        shift ;;
+      -j|--job)
+        JOB_NAME=$2
+        shift 2
+        ;;
+      -f|--filter)
+        FILTER_NAME=$2
+        shift 2
+        ;;
+      -m|--mappers)
+        NUM_MAPPERS=$2
+        shift 2
+        ;;
+      -r|--reducers)
+        NUM_REDUCERS=$2
+        shift 2
+        ;;
+      -p|--pull-force)
+        PULL=""
+        shift
+        ;;
+      -h|--help)
+        show_usage
+        ;;
       --) shift ; break ;;
       *) echo "Internal error: $1" ; exit 1 ;;
     esac
@@ -58,34 +78,22 @@ function set_parameters {
   echo "FILTER_NAME = $FILTER_NAME"
   echo "NUM_MAPPERS = $NUM_MAPPERS"
   echo "NUM_REDUCERS = $NUM_REDUCERS"
-  echo "LOCAL = $LOCAL"
+  echo "PULL = $PULL"
 }
 
 function run_job {
   printf "\n------> Starting job\n"
   cd ~/telemetry-server
 
-  if [ $LOCAL ]; then
-    python -m mapreduce.job ../fennec-telemetry/jobs/$JOB_NAME.py \
-      --input-filter ../fennec-telemetry/filters/$FILTER_NAME.json \
-      --num-mappers $NUM_MAPPERS \
-      --num-reducers $NUM_REDUCERS \
-      --data-dir /mnt/telemetry/work/cache \
-      --work-dir /mnt/telemetry/work \
-      --output $OUTPUT_FILE \
-      --bucket "telemetry-published-v1" \
-      --local-only
-  else
-    python -m mapreduce.job ../fennec-telemetry/jobs/$JOB_NAME.py \
-      --input-filter ../fennec-telemetry/filters/$FILTER_NAME.json \
-      --num-mappers $NUM_MAPPERS \
-      --num-reducers $NUM_REDUCERS \
-      --data-dir /mnt/telemetry/work \
-      --work-dir /mnt/telemetry/work \
-      --output $OUTPUT_FILE \
-      --bucket "telemetry-published-v1"
-  fi
-
+  python -m mapreduce.job ../fennec-telemetry/jobs/$JOB_NAME.py \
+    --input-filter ../fennec-telemetry/filters/$FILTER_NAME.json \
+    --num-mappers $NUM_MAPPERS \
+    --num-reducers $NUM_REDUCERS \
+    --data-dir /mnt/telemetry/work/cache \
+    --work-dir /mnt/telemetry/work \
+    --output $OUTPUT_FILE \
+    --bucket "telemetry-published-v1" \
+    $PULL
 
   printf "\n------> Results in $OUTPUT_FILE\n"
   cat $OUTPUT_FILE
