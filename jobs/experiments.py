7c21073a6e2f77632f66c7a7b9c84c9b98a12f4d
Mark Finkle <mark.finkle@gmail.com>
2015-12-22 11:15:08 -0500

    Add format conversions and friendly name conversions to experiments

--- jobs/experiments.py
+++ jobs/experiments.py
@@ -69,6 +69,10 @@ def reduce(key, value, cx):
     cx.write(key, value_all)
 
 def add_to_events(key, events, event):
+  action = ""
+  if event["action"] is not None:
+    action = str(event["action"])
+
   method = ""
   if event["method"] is not None:
     method = str(event["method"])
@@ -76,10 +80,55 @@ def add_to_events(key, events, event):
   extras = ""
   if "extras" in event and event["extras"] is not None:
     extras = str(event["extras"])
+    # Anonymize some private tab actions
+    if action == "action.1":
+      if extras == "new_private_tab":
+        extras = "new_tab"
+      elif extras == "home_open_private_tab":
+        extras = "home_open_new_tab"
+      elif extras == "web_open_private_tab":
+        extras = "web_open_new_tab"
+      elif extras == "close_private_tabs":
+        extras = "close_all_tabs"
+
+  # Cleanup setdefault.1 data to match new format
+  if action == "setdefault.1":
+    action = "panel.setdefault.1"
+    method = "dialog"
+
+  # Cleanup save.1 data to match new format
+  if action == "save.1":
+    if method == "button" and extras.startswith("firstrun-import-dialog-"):
+      action = "action.1"
+    elif method == "button" and extras == "reader":
+      extras = "reading_list"
+    elif method == "pageaction" and extras == "reader":
+      extras = "reading_list"
+
+  # Cleanup homescreen data to match new format
+  if action == "search.1" and method == "homescreen" and extras == "history":
+    method = "suggestion"
+  elif action == "action.1" and method == "homescreen" and extras == "hint-private-browsing":
+    method = "panel"
+
+  # Use friendly names for panel events
+  if action.startswith("panel."):
+    if "4becc86b-41eb-429a-a042-88fe8b5a094e" in extras:
+      extras = "top_sites"
+    elif "7f6d419a-cd6c-4e34-b26f-f68b1b551907" in extras:
+      extras = "bookmarks"
+    elif "20f4549a-64ad-4c32-93e4-1dcef792733b" in extras:
+      extras = "reading_list"
+    elif "f134bf20-11f7-4867-ab8b-e8e705d7fbe8" in extras:
+      extras = "history"
+    elif "5c2601a5-eedc-4477-b297-ce4cef52adf8" in extras:
+      extras = "recent_tabs"
+    elif "72429afd-8d8b-43d8-9189-14b779c563d0" in extras:
+      extras = "remote_tabs"
 
   for session in event["sessions"]:
     if "experiment." in session:
-      identifier = "{0},{1},{2},{3},{4}".format(key, session, str(event["action"]), method, extras)
+      identifier = "{0},{1},{2},{3},{4}".format(key, session, action, method, extras)
       if not identifier in events:
         events[identifier] = 0
       events[identifier] += 1
